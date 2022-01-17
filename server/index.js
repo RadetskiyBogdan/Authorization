ad29f537baab5590fc2e454373093af2533b3a2d
Bogdan R <bogdan20226@gmail.com>
2022-01-17 14:37:40 +0200

    workAuth

--- server/index.js
+++ server/index.js
@@ -1,22 +1,28 @@
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-
-const db = require('./db')
-
-const app = express()
-const apiPort = 3000
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
-
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+//import routes
+const authRoutes = require('./routes/auth');
+const { db } = require('./models/User');
+//app
+const app = express();
+// db
+mongoose
+  .connect(process.env.DATABASE,{
+      useNewUrlParser: true,
+      //useCreateIndex: true,
+      //useUnifiedTopology: true
+   })
+  .then(() => console.log('DB Connected'));
+//middlewares
+app.use(bodyParser.json());
+app.use(cors());
+//routes middleware
+app.use('/api', authRoutes);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`)
+});
